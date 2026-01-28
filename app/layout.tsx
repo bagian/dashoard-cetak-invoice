9ef486cff7533e7dc185fe5f363c9012e1c8358a
Gilang Ramadhan <45790050+9Alpha9@users.noreply.github.com>
2026-01-28 08:41:44 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import {ToastProvider} from "@/components/providers/toast-provider";
+import {ModalProvider} from "@/components/providers/modal-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ToastProvider>
+          <ModalProvider>{children}</ModalProvider>
+        </ToastProvider>
       </body>
     </html>
   );
